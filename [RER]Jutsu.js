73a6be4b4b59d51c9903189d4bc6bb8befb2c14d
respectZ <respectZ@users.noreply.github.com>
2015-06-07 12:53:39 +0700

    Update [RER]Jutsu.js

--- [RER]Jutsu.js
+++ [RER]Jutsu.js
@@ -1,13 +1,14 @@
-var tick=40;
+var tick = 40;
+var fireTick = 120;
 var yaw;
 var tkexp = 1000;
 var kexp = 0;
 var klvl = 1;
-
+       
 function modTick() {
-tick--;
+        tick--;
 yaw = getYaw();
-if(yaw>360) {
+         if(yaw>360) {
 yaw -=360;
 Entity.setRot(getPlayerEnt(),yaw,getPitch());
 }
@@ -16,95 +17,142 @@ yaw +=360;
 Entity.setRot(getPlayerEnt(),yaw,getPitch());
 }
 }
-
+ 
 function procCmd(cmd) {
-var c = cmd.split(" ");
-if(c[0]=="fire") {
-clientMessage("Fire Jutsu !");
-Jutsu("fire",Player.getX(),Player.getY(),Player.getZ());
-}
-if(c[0]=="wind") {
-clientMessage("Wind Jutsu !");
-Jutsu("wind",Player.getX(),Player.getY(),Player.getZ());
+        if(cmd == "fire") {
+                clientMessage("Fire Jutsu !");
+                 Jutsu(getPlayerEnt(),Player.getX(),Player.getY(),Player.getZ());
+        } else if(cmd == "wind") {
+        clientMessage("Wind Jutsu !");
+        windJutsu(getPlayerEnt(),Player.getX(),Player.getY(),Player.getZ());
 }
 }
 
-function Jutsu(type,x,y,z) {
-if(type=="fire") {
-if(yaw>=45&&yaw<=135) {
-if(getTile(x-1,y-1,z)==0)
-setTile(x-1,y-1,z,51);
-if(getTile(x-2,y-1,z)==0)
-setTile(x-2,y-1,z,51);
-if(getTile(x-3,y-1,z)==0)
-setTile(x-3,y-1,z,51);
-if(getTile(x-4,y-1,z)==0)
-setTile(x-4,y-1,z,51);
-if(getTile(x-5,y-1,z)==0)
-setTile(x-5,y-1,z,51);
-if(getTile(x-6,y-1,z)==0)
-setTile(x-6,y-1,z,51);
-}
-if(yaw>=135&&yaw<=225) {
-if(getTile(x,y-1,z-1)==0)
-setTile(x,y-1,z-1,51);
-if(getTile(x,y-1,z-2)==0)
-setTile(x,y-1,z-2,51);
-if(getTile(x,y-1,z-3)==0)
-setTile(x,y-1,z-3,51);
-if(getTile(x,y-1,z-4)==0)
-setTile(x,y-1,z-4,51);
-if(getTile(x,y-1,z-5)==0)
-setTile(x,y-1,z-5,51);
-if(getTile(x,y-1,z-6)==0)
-setTile(x,y-1,z-6,51);
-}
-if(yaw>=225&&yaw<=315) {
-if(getTile(x+1,y-1,z)==0)
-setTile(x+1,y-1,z,51);
-if(getTile(x+2,y-1,z)==0)
-setTile(x+2,y-1,z,51);
-if(getTile(x+3,y-1,z)==0)
-setTile(x+3,y-1,z,51);
-if(getTile(x+4,y-1,z)==0)
-setTile(x+4,y-1,z,51);
-if(getTile(x+5,y-1,z)==0)
-setTile(x+5,y-1,z,51);
-if(getTile(x+6,y-1,z)==0)
-setTile(x+6,y-1,z,51);
-}
-if(yaw>=315||yaw<=45) {
+ 
+function Jutsu(e,x,y,z) {
+        if(yaw>=45&&yaw<=135) {
+                if(getTile(x-1,y-1,z)==0 && fireTick == 120)
+                        setTile(x-1,y-1,z,51)
+                if(getTile(x-2,y-1,z)==0 && fireTick == 100)
+                        setTile(x-2,y-1,z,51)
+                if(getTile(x-3,y-1,z)==0 && fireTick == 80)
+                setTile(x-3,y-1,z,51)
+        if(getTile(x-4,y-1,z)==0 && fireTick == 60)
+                setTile(x-4,y-1,z,51)
+        if(getTile(x-5,y-1,z)==0 && fireTick == 40)
+                setTile(x-5,y-1,z,51)
+        if(getTile(x-6,y-1,z)==0 && fireTick == 20)
+                setTile(x-6,y-1,z,51)
+        if(getTile(x-7,y-1,z)==0 && fireTick == 0)
+                setTile(x-7,y-1,z,51)
+        fireTick = 120;
+        if(getTile(x-1,y-1,z)==51 && fireTick == 120)
+                        setTile(x-1,y-1,z,false)
+                if(getTile(x-2,y-1,z)==51 && fireTick == 100)
+                        setTile(x-2,y-1,z,false)
+                if(getTile(x-3,y-1,z)==51 && fireTick == 80)
+                setTile(x-3,y-1,z,false)
+        if(getTile(x-4,y-1,z)==51 && fireTick == 60)
+                setTile(x-4,y-1,z,false)
+        if(getTile(x-5,y-1,z)==51 && fireTick == 40)
+                setTile(x-5,y-1,z,false)
+        if(getTile(x-6,y-1,z)==51 && fireTick == 20)
+                setTile(x-6,y-1,z,false)
+        if(getTile(x-7,y-1,z)==51 && fireTick == 0)
+                setTile(x-7,y-1,z,false)
+               
+                }       if(yaw>=135&&yaw<=225) {
+                if(getTile(x,y-1,z-1)==0 && fireTick == 120)
+                        setTile(x,y-1,z-1,51)
+                if(getTile(x,y-1,z-2)==0 && fireTick == 100)
+                        setTile(x,y-1,z-2,51)
+                if(getTile(x,y-1,z-3)==0 && fireTick == 80)
+                setTile(x,y-1,z-3,51)
+        if(getTile(x,y-1,z-4)==0 && fireTick == 60)
+                setTile(x,y-1,z-4,51)
+        if(getTile(x,y-1,z-5)==0 && fireTick == 40)
+                setTile(x,y-1,z-5,51)
+        if(getTile(x,y-1,z-6)==0 && fireTick == 20)
+                setTile(x,y-1,z-6,51)
+        if(getTile(x,y-1,z-7)==0 && fireTick == 0)
+                setTile(x,y-1,z-7,51)
+        fireTick = 120;
+        if(getTile(x,y-1,z-1)==51 && fireTick == 120)
+                        setTile(x,y-1,z-1,false)
+                if(getTile(x,y-1,z-2)==51 && fireTick == 100)
+                        setTile(x,y-1,z-2,false)
+                if(getTile(x,y-1,z-3)==51 && fireTick == 80)
+                setTile(x,y-1,z-3,false)
+        if(getTile(x,y-1,z-4)==51 && fireTick == 60)
+                setTile(x,y-1,z-4,false)
+        if(getTile(x,y-1,z-5)==51 && fireTick == 40)
+                setTile(x,y-1,z-5,false)
+        if(getTile(x,y-1,z-6)==51 && fireTick == 20)
+                setTile(x,y-1,z-6,false)
+        if(getTile(x,y-1,z-7)==51 && fireTick == 0)
+                setTile(x,y-1,z-7,false)
+               
+                }       if(yaw>=225&&yaw<=315) {
+                        if(getTile(x+1,y-1,z)==0 && fireTick == 120)
+                        setTile(x+1,y-1,z,51)
+                if(getTile(x+2,y-1,z)==0 && fireTick == 100)
+                        setTile(x+2,y-1,z,51)
+                if(getTile(x+3,y-1,z)==0 && fireTick == 80)
+                setTile(x+3,y-1,z,51)
+        if(getTile(x+4,y-1,z)==0 && fireTick == 60)
+                setTile(x+4,y-1,z,51)
+        if(getTile(x+5,y-1,z)==0 && fireTick == 40)
+                setTile(x+5,y-1,z,51)
+        if(getTile(x+6,y-1,z)==0 && fireTick == 20)
+                setTile(x+6,y-1,z,51)
+        if(getTile(x+7,y-1,z)==0 && fireTick == 0)
+                setTile(x+7,y-1,z,51)
+        fireTick = 120;
+        if(getTile(x+1,y-1,z)==51 && fireTick == 120)
+                        setTile(x+1,y-1,z,false)
+                if(getTile(x+2,y-1,z)==51 && fireTick == 100)
+                        setTile(x+2,y-1,z,false)
+                if(getTile(x+3,y-1,z)==51 && fireTick == 80)
+                setTile(x+3,y-1,z,false)
+        if(getTile(x+4,y-1,z)==51 && fireTick == 60)
+                setTile(x+4,y-1,z,false)
+        if(getTile(x+5,y-1,z)==51 && fireTick == 40)
+                setTile(x+5,y-1,z,false)
+        if(getTile(x+6,y-1,z)==51 && fireTick == 20)
+                setTile(x+6,y-1,z,false)
+        if(getTile(x+7,y-1,z)==51 && fireTick == 0)
+                setTile(x+7,y-1,z,false)
+                }       if(yaw>=315||yaw<=45) {
 if(getTile(x,y-1,z+1)==0)
-setTile(x,y-1,z+1,51);
-if(getTile(x,y-1,z+2)==0)
-setTile(x,y-1,z+2,51);
-if(getTile(x,y-1,z+3)==0)
-setTile(x,y-1,z+3,51);
-if(getTile(x,y-1,z+4)==0)
-setTile(x,y-1,z+4,51);
-if(getTile(x,y-1,z+5)==0)
-setTile(x,y-1,z+5,51);
-if(getTile(x,y-1,z+6)==0)
-setTile(x,y-1,z+6,51);
-}
-}
-if(type=="wind") {
-if(Player.getPointedEntity(getPlayerEnt())>0&&Entity.getEntityTypeId(Player.getPointedEntity(getPlayerEnt()))>0&&Entity.getEntityTypeId(Player.getPointedEntity(getPlayerEnt()))<43) {
-knock(Player.getPointedEntity(getPlayerEnt()),yaw);
-}
-}
-}
+                if(getTile(x,y-1,z+1)==0 && fireTick == 120)
+                        setTile(x,y-1,z+1,51)
+                if(getTile(x,y-1,z+2)==0 && fireTick == 100)
+                        setTile(x,y-1,z+2,51)
+                if(getTile(x,y-1,z+3)==0 && fireTick == 80)
+                setTile(x,y-1,z+3,51)
+        if(getTile(x,y-1,z+4)==0 && fireTick == 60)
+                setTile(x,y-1,z+4,51)
+        if(getTile(x,y-1,z+5)==0 && fireTick == 40)
+                setTile(x,y-1,z+5,51)
+        if(getTile(x,y-1,z+6)==0 && fireTick == 20)
+                setTile(x,y-1,z+6,51)
+        if(getTile(x,y-1,z+7)==0 && fireTick == 0)
+                setTile(x,y-1,z+7,51)
+        fireTick = 120;
+        if(getTile(x,y-1,z+1)==51 && fireTick == 120)
+                        setTile(x,y-1,z+1,false)
+                if(getTile(x,y-1,z+2)==51 && fireTick == 100)
+                        setTile(x,y-1,z+2,false)
+                if(getTile(x,y-1,z+3)==51 && fireTick == 80)
+                setTile(x,y-1,z+3,false)
+        if(getTile(x,y-1,z+4)==51 && fireTick == 60)
+                setTile(x,y-1,z+4,false)
+        if(getTile(x,y-1,z+5)==51 && fireTick == 40)
+                setTile(x,y-1,z+5,false)
+        if(getTile(x,y-1,z+6)==51 && fireTick == 20)
+                setTile(x,y-1,z+6,false)
+        if(getTile(x,y-1,z+7)==51 && fireTick == 0)
+                setTile(x,y-1,z+7,false)
+                }
 
-var all = [];
-function entityAddedHook(e) {
-all.push(e);
-}
-
-function knock(e,yaw1) {
-var velocity = yaw1+90;
-var velX = Math.cos(yaw1*(Math.PI/180));
-var velZ = Math.sin(yaw1*(Math.PI/180));
-setVelX(e, velX*1.5);
-setVelY(e, 1);
-setVelZ(e, velZ*1.5);
-}
+                    }
